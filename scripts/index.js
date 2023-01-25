f53425456a2cefa63ce5d16cba9855dfd917fba4
thayrov <thayrovg@gmail.com>
2023-01-25 11:41:36 -0600

    correct encrypting functions so it doesn't process numbers and alternate characters; extra adjusted some minor styles

--- scripts/index.js
+++ scripts/index.js
@@ -5,20 +5,19 @@ const desencriptado = {enter: 'e', imes: 'i', ai: 'a', ober: 'o', ufat: 'u'};
 const encriptarTexto = start => {
 	start.preventDefault();
 	var inputUsuario = document.getElementById('inputArea').value;
-	var resultadoEncriptado = inputUsuario.replace(
-		/[eiaou]/g,
-		i => encriptado[i],
-	);
+	var resultadoEncriptado = inputUsuario
+		.replace(/[eiaou]/g, i => encriptado[i])
+		.replace(/[^a-z]/g, '');
+
 	document.getElementById('resultado').innerHTML = resultadoEncriptado;
 };
 
 const desencriptarTexto = start => {
 	start.preventDefault();
 	var inputUsuario = document.getElementById('inputArea').value;
-	var resultadoDesencriptado = inputUsuario.replace(
-		/(enter)+|(imes)+|(ai)+|(ober)+|(ufat)+/g,
-		i => desencriptado[i],
-	);
+	var resultadoDesencriptado = inputUsuario
+		.replace(/(enter)+|(imes)+|(ai)+|(ober)+|(ufat)+/g, i => desencriptado[i])
+		.replace(/[^a-z]/g, '');
 	document.getElementById('resultado').innerHTML = resultadoDesencriptado;
 };
 
